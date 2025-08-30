9262b1774abbb9df797afabb2e4e93419d26c184
Vrushank Patel <vrushankpatel5@gmail.com>
2025-08-30 17:45:12 +0530

    Removed sudo force

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 print_help() {
     cat << "EOF"
- ______  ___   ____   _____ 
-/_  __/ /   | / __ \ / ___/
- / /   / /| |/ /_/ / \__ \ 
-/ /   / ___ / _, _/ ___/ / 
-/_/   /_/  |/_/ |_| /____/  
+  ______  ___      ____   _____ 
+ /_  __/ /   |    / __ \ / ___/
+  / /   / /| |   / /_/ / \__ \ 
+ / /   / ___ |  / _, _/ ___/ / 
+/_/   /_/  |_| /_/ |_| /____/  
 
 TARS Application Manager
 ------------------------
@@ -16,7 +16,7 @@ DESCRIPTION:
     applications or all applications at once.
 
 USAGE:
-    sudo ./start.sh <app_name | all | --help>
+    ./start.sh <app_name | all | --help>
 
 OPTIONS:
     app_name    Name of the application to start (must exist in apps directory)
@@ -24,8 +24,8 @@ OPTIONS:
     --help      Display this help message
 
 EXAMPLES:
-    sudo ./start.sh immich     # Start the immich application
-    sudo ./start.sh all        # Start all available applications
+    ./start.sh immich     # Start the immich application
+    ./start.sh all        # Start all available applications
 
 DIRECTORY STRUCTURE:
     The script expects applications to be organized as follows:
@@ -37,7 +37,6 @@ DIRECTORY STRUCTURE:
     └── ...
 
 NOTE:
-    - This script requires sudo privileges
     - Each application must have a start script named: start-<app_name>.sh
     - All start scripts will be made executable automatically
 EOF
@@ -47,13 +46,6 @@ EOF
 if [ "$1" == "--help" ] || [ $# -eq 0 ]; then
     print_help
     exit 0
-fi
-
-# Check if script is run as root/sudo
-if [ "$EUID" -ne 0 ]; then 
-    echo "Please run as sudo"
-    echo "Use --help for more information"
-    exit 1
 fi
 
 # Store the argument
@@ -68,18 +60,12 @@ fi
 # Function to execute single app script
 execute_app_script() {
     local app=$1
-    local base_path
-    if [ -n "$SUDO_USER" ]; then
-        base_path="/home/$SUDO_USER/TARS"
-    else
-        base_path="$PWD"
-    fi
-    local script_path="$base_path/apps/${app}/start-${app}.sh"
+    local script_path="apps/${app}/start-${app}.sh"
     
     if [ -f "$script_path" ]; then
         echo "Executing $script_path"
         chmod +x "$script_path"
-        cd "$(dirname "$script_path")" && sudo -u "$SUDO_USER" ./$(basename "$script_path")
+        cd "$(dirname "$script_path")" && ./$(basename "$script_path")
     else
         echo "Error: Script not found at $script_path"
         exit 1
